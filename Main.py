cd5a304fcff60defec8b7737e59b6dafdb89a315
Brandon Leung <brandonmichael.kg@gmail.com>
2018-12-10 23:17:04 -0500

    sanitize and save table output

--- Main.py
+++ Main.py
@@ -12,6 +12,8 @@ base_url = "http://www.espn.com/"
 # input: start will be an integer indicating how many past games
 # input: end will be in integer indicating how many future games
 def get_schedule(team, start, end):
+    schedule = []
+
     team_code = DataDictionary.team_codes.get(team)
 
     # generate the url
@@ -24,10 +26,14 @@ def get_schedule(team, start, end):
     # gather the html -- formatted
     soup = BeautifulSoup(page, 'html.parser')
 
-    # go through the table... process later
+    # go through the table and grab dates, opponent, score or time
     for tr in soup.find_all('tr')[4:]:
         tds = tr.find_all('td')
-        print(tds[0].text, tds[1].text, tds[2].text)
+
+        schedule.append([tds[0].text, tds[1].text.strip('vs@ '), tds[2].text.strip()])
+
+    for line in schedule:
+        print(str(line) + "\n")
 
 
-get_schedule('houstonrockets', None, None)
+get_schedule('torontoraptors', None, None)
