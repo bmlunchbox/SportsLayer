6a86ba82bc0efe13e4e7b92accb5d8f556b2d2af
Brandon Leung <brandonmichael.kg@gmail.com>
2018-12-10 23:06:14 -0500

    logic for retrieving raw schedule data

--- Main.py
+++ Main.py
@@ -4,27 +4,30 @@ import DataDictionary
 from bs4 import BeautifulSoup
 
 
-# check schedule
-def get_schedule(team, start, end):
-    # will be passed in as a query
-    team_code = DataDictionary.teamCodes.get(team)
+base_url = "http://www.espn.com/"
 
-    # http request
-    page_url = 'http://www.espn.com/nba/team/schedule/_/name/'
+
+# default to return next game: team, game
+# input: team name as a string
+# input: start will be an integer indicating how many past games
+# input: end will be in integer indicating how many future games
+def get_schedule(team, start, end):
+    team_code = DataDictionary.team_codes.get(team)
+
+    # generate the url
+    page_url = base_url + 'nba/team/schedule/_/name/'
     page_url += str(team_code)
 
-    page = urllib.request.urlopen(page_url)
+    # create the request
+    page = urllib.request.urlopen(page_url).read()
 
-    soup = BeautifulSoup(page.read(), 'html.parser')
+    # gather the html -- formatted
+    soup = BeautifulSoup(page, 'html.parser')
 
-    print(soup.prettify())
+    # go through the table... process later
+    for tr in soup.find_all('tr')[4:]:
+        tds = tr.find_all('td')
+        print(tds[0].text, tds[1].text, tds[2].text)
 
 
-get_schedule("torontoraptors", None, None)
-
-# soup = BeautifulSoup(html_doc, 'html.parser')
-
-# test file
-# file = open("test.txt", "w")
-
-# file.write(str(page.read()))
+get_schedule('houstonrockets', None, None)
