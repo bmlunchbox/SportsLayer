e72c8baaf84ce07a86fa14621f6bd82f288cd6d4
Brandon Leung <brandonmichael.kg@gmail.com>
2018-12-25 19:03:09 -0500

    initial commit for new functions

---
+++ Controllers/StandingsController.py
@@ -0,0 +1,17 @@
+from Data import SoupService
+
+
+def get_conference_standings(conference):
+    return ""
+
+
+def get_all():
+    return ""
+
+
+def get_playoff_teams(conference):
+    return ""
+
+
+def get_team(team):
+    return ""
---
+++ Controllers/TeamController.py
@@ -0,0 +1,9 @@
+from Data import SoupService
+
+
+def get_stat_line(team):
+    return ""
+
+
+def get_advanced_stat_line(team):
+    return ""
