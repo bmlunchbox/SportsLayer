d67ea59f56ab80d43a770c3a7f6b041e727f26e0
Brandon Leung <brandonmichael.kg@gmail.com>
2018-12-27 17:59:40 -0500

    added dictionary for user input to standings table

--- Data/DataDictionary.py
+++ Data/DataDictionary.py
@@ -15,29 +15,30 @@ team_codes = {
     'houston': 'hou',
     'indiana': 'ind',
     'la': 'lac',
-    'losangeles': 'lal',
+    'los angeles': 'lal',
     'memphis': 'mem',
     'miami': 'mia',
     'milwaukee': 'mil',
     'minnesota': 'min',
-    'neworleans': 'no',
-    'newyork': 'ny',
-    'oklahomacity': 'okc',
+    'new orleans': 'no',
+    'new york': 'ny',
+    'oklahoma city': 'okc',
     'orlando': 'orl',
     'philadelphia': 'phi',
     'phoenix': 'phx',
     'portland': 'por',
     'sacramento': 'sac',
-    'sanantonio': 'sa',
+    'san antonio': 'sa',
     'toronto': 'tor',
     'utah': 'utah',
     'washington': 'wsh'
 }
 
+# from ESPN table name to output string
 short_names = {
     'atlanta': 'Hawks',
     'boston': 'Celtics',
-    'brooklyn': 'Brooklyn',
+    'brooklyn': 'Nets',
     'charlotte': 'Hornets',
     'chicago': 'Bulls',
     'cleveland': 'Cavaliers',
@@ -65,4 +66,39 @@ short_names = {
     'toronto': 'Raptors',
     'utah': 'Jazz',
     'washington': 'Wizards'
+}
+
+# from user input to full team name
+# depreciate once have team name variation sets
+nba_string = {
+    'atlanta': 'Atlanta Hawks',
+    'boston': 'Boston Celtics',
+    'brooklyn': 'Brooklyn Nets',
+    'charlotte': 'Charlotte Hornets',
+    'chicago': 'Chicago Bulls',
+    'cleveland': 'Cleveland Cavaliers',
+    'dallas': 'Dallas Mavericks',
+    'denver': 'Denver Nuggets',
+    'detroit': 'Detroit Pistons',
+    'golden state': 'Golden State Warriors',
+    'houston': 'Houston Rockets',
+    'indiana': 'Indiana Pacers',
+    'la': 'LA Clippers',
+    'los angeles': 'Los Angeles Lakers',
+    'memphis': 'Memphis Grizzlies',
+    'miami': 'Miami Heat',
+    'milwaukee': 'Milwaukee Bucks',
+    'minnesota': 'Minnesota Timberwolves',
+    'new orleans': 'New Orleans Pelicans',
+    'new york': 'New York Knicks',
+    'oklahoma city': 'Oklahoma City Thunder',
+    'orlando': 'Orlando Magic',
+    'philadelphia': 'Philadelphia 76ers',
+    'phoenix': 'Phoenix Suns',
+    'portland': 'Portland Trailblazers',
+    'sacramento': 'Sacramento Kings',
+    'san antonio': 'San Antonio Spurs',
+    'toronto': 'Toronto Raptors',
+    'utah': 'Utah Jazz',
+    'washington': 'Washington Wizards'
 }
